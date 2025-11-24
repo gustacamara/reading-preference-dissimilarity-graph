01e26dd091f0602cb625fb5e975d7f7036168198
Reshi <duuhmorciani@gmail.com>
2025-11-24 01:32:43 -0300

    Implement GraphController and add all methods to verify the properties of the graph

---
+++ src/Controller/GraphController.java
@@ -0,0 +1,27 @@
+package Controller;
+
+import Model.Graph;
+
+public class GraphController {
+    public void graph(Graph graph) {
+        boolean isConnected = graph.isConnected();
+        boolean isEulerian = graph.isEulerian();
+
+        if (isConnected) {
+            System.out.println("The graph is connected!");
+        } else {
+            System.out.println("The graph is not connected!");
+        }
+
+        if (isEulerian){
+            System.out.println("The graph is Eulerian!");
+        } else {
+            System.out.println("The graph is not Eulerian!");
+        }
+
+        graph.isCyclic();
+
+        graph.betweennessCentrality();
+        graph.closenessCentrality();
+    }
+}
