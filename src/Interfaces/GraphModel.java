20449e9169f0cbe4c21665dfbcdc33721c3332a0
Reshi <duuhmorciani@gmail.com>
2025-11-24 01:14:44 -0300

    Add methods to GhaphModel

--- src/Interfaces/GraphModel.java
+++ src/Interfaces/GraphModel.java
@@ -2,7 +2,22 @@ package Interfaces;
 
 import Model.Vertex;
 
+import java.util.List;
+import java.util.Map;
+
 public interface GraphModel {
     public void setVertices(Vertex[] vertices);
     public void setLabels(String[] labels);
+    void searchComponents(int start, List<Integer> components, List<Integer> visited);
+    int components();
+    boolean isConnected();
+    void calculateDegree(Map<String, Integer> countDegree);
+    boolean isEulerian ();
+    boolean dfsCycle(int start, List<Integer> visited);
+    void isCyclic();
+    int[] dijkstra(int start, double[] distanceValues);
+    double sumDistances(int start);
+    void closenessCentrality ();
+    void betweennessCentrality();
+    void printGraph();
 }
