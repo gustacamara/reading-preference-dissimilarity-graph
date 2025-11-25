545edd07aa01b234074e18c5995630bf07ba4b8b
Reshi <duuhmorciani@gmail.com>
2025-11-25 01:56:00 -0300

    Update RandomGraphController to accept user input

--- src/Controller/RandomGraphController.java
+++ src/Controller/RandomGraphController.java
@@ -5,52 +5,67 @@ import Model.RandomGraph;
 import java.util.Scanner;
 
 public class RandomGraphController {
+
     private int numVertices;
-    private int numEdges;
+    int numEdges;
     private String weights;
     String[] splitWeights;
 
-    public void input() {
+    public void randomGraph(RandomGraph randomGraph) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the number of vertices: ");
         numVertices = sc.nextInt();
 
-        System.out.println("Enter the number of edges: ");
-        numEdges = sc.nextInt();
+        String option;
 
-        sc.nextLine();
+        while(true) {
+            System.out.println("Enter the number of edges: ");numEdges = sc.nextInt();
+
+            System.out.println("Do you want generate a connected graph? (Y - yes, N - no ): ");
+
+            sc.nextLine();
+
+            option = sc.nextLine().trim().toUpperCase();
+
+            if (option.equals("Y") && numEdges < numVertices - 1) {
+                System.out.println("Error: For a connected graph, the number of edges must be at least " + (numVertices - 1) + ".");
+                continue;
+            }
+
+            break;
+        }
 
         System.out.println("Enter the weights (comma-separated): ");
+
         weights = sc.nextLine();
-
-    }
-
-    public void randomGraph(RandomGraph randomGraph) {
-        input();
-
-        int vertices = this.numVertices;
-        int edges = this.numEdges;
-        String weights = this.weights;
 
         splitWeights = weights.split("\\s*,\\s*");
 
-        System.out.println("\nConnected weighted graph: ");
+        Graph graph = null;
 
-        Graph connectedWeightedGraph = randomGraph.connectedWeightedGraph(vertices, edges, splitWeights);
-        connectedWeightedGraph.printGraph();
+        switch (option) {
+            case "Y":
+                System.out.println("\nConnected weighted graph: ");
+                graph = randomGraph.generateConnectedGraph(numVertices, numEdges, splitWeights);
+                break;
 
-        boolean isConnected = connectedWeightedGraph.isConnected();
+            case "N":
+                System.out.println("\nUnconnected weighted graph: ");
+                graph = randomGraph.generateUnconnectedGraph(numVertices, numEdges, splitWeights);
+                break;
 
-        if (isConnected) {
+            default:
+                System.out.println("Invalid option!");
+                break;
+        }
+
+        graph.printGraph();
+
+        if (graph.isConnected()) {
             System.out.println("The graph is connected!");
         } else {
             System.out.println("The graph is not connected!");
         }
-
-        System.out.println("\nUnconnected weighted graph: ");
-
-        Graph unconnectedWeightedGraph = randomGraph.generateWeightedGraph(vertices, edges, splitWeights);
-        unconnectedWeightedGraph.printGraph();
     }
 }
--- src/Model/RandomGraph.java
+++ src/Model/RandomGraph.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class RandomGraph {
 
-    public void generateWeightedEdges (Graph graph, int numVertices, int numEdges, int[] listWeight) {
+    public void generateEdges (Graph graph, int numVertices, int numEdges, double[] listWeight) {
         Random rand = new Random();
         int countEdges = 0;
 
@@ -20,7 +20,7 @@ public class RandomGraph {
                 continue;
             }
 
-            int weight = listWeight[rand.nextInt(listWeight.length)];
+            double weight = listWeight[rand.nextInt(listWeight.length)];
 
             graph.setAdj(start, target, weight);
             countEdges++;
@@ -28,105 +28,50 @@ public class RandomGraph {
 
     }
 
-    public void generateUnweightedEdges (Graph graph, int numVertices, int numEdges) {
-        Random rand = new Random();
-        int countEdges = 0;
-
-        while (countEdges < numEdges) {
-            int start = rand.nextInt(numVertices);
-            int target = rand.nextInt(numVertices);
-
-            if (start == target) {
-                continue;
-            }
-
-            if (graph.existEdge(start, target)) {
-                continue;
-            }
-
-            graph.setAdj(start, target, 0);
-            countEdges++;
-        }
-    }
-
-    public Graph generateWeightedGraph (int numVertices, int numEdges, String[] splitWeights) {
+    public Graph generateUnconnectedGraph (int numVertices, int numEdges, String[] splitWeights) {
         Graph randomGraph = new Graph(numVertices);
 
-        int[] listWeight = new int[splitWeights.length];
+        double[] listWeight = new double[splitWeights.length];
 
         for (int l = 0; l < numVertices; l ++) {
             randomGraph.setLabels(l, String.valueOf(l +1));
         }
 
         for (int w = 0; w < splitWeights.length; w++) {
-            listWeight[w] = Integer.parseInt(splitWeights[w].trim());
+            listWeight[w] = Double.parseDouble(splitWeights[w].trim());
         }
 
-        generateWeightedEdges(randomGraph, numVertices, numEdges, listWeight);
+        generateEdges(randomGraph, numVertices, numEdges, listWeight);
 
         return randomGraph;
     }
 
-    public Graph generateUnweightedGraph (int numVertices, int numEdges) {
-        Graph randomGraph = new Graph(numVertices);
 
-        for (int l = 0; l < numVertices; l ++) {
-            randomGraph.setLabels(l, String.valueOf(l +1));
-        }
 
-        generateUnweightedEdges(randomGraph, numVertices, numEdges);
-
-        return randomGraph;
-    }
-
-    public Graph connectedWeightedGraph (int numVertices, int numEdges, String[] splitWeights) {
+    public Graph generateConnectedGraph (int numVertices, int numEdges, String[] splitWeights) {
         Graph connectedGraph = new Graph(numVertices);
         Random rand = new Random();
 
-        int[] listWeight = new int[splitWeights.length];
+        double[] listWeight = new double[splitWeights.length];
 
         for (int l = 0; l < numVertices; l ++) {
             connectedGraph.setLabels(l, String.valueOf(l +1));
         }
 
         for (int w = 0; w < splitWeights.length; w++) {
-            listWeight[w] = Integer.parseInt(splitWeights[w].trim());
+            listWeight[w] = Double.parseDouble(splitWeights[w].trim());
         }
 
         for (int i = 1; i < numVertices; i++) {
             int parent = rand.nextInt(i);
-            int weight = listWeight[rand.nextInt(listWeight.length)];
+            double weight = listWeight[rand.nextInt(listWeight.length)];
             connectedGraph.setAdj(parent, i, weight);
         }
 
         int remainingEdges = numEdges - (numVertices - 1);
 
         if (remainingEdges > 0) {
-            generateWeightedEdges(connectedGraph,numVertices,remainingEdges,listWeight);
-        }
-
-        return connectedGraph;
-    }
-
-    public Graph connectedUnweightedGraph (int numVertices, int numEdges) {
-        Graph connectedGraph = new Graph(numVertices);
-        Random rand = new Random();
-
-
-        for (int l = 0; l < numVertices; l ++) {
-            connectedGraph.setLabels(l, String.valueOf(l +1));
-        }
-
-
-        for (int i = 1; i < numVertices; i++) {
-            int parent = rand.nextInt(i);
-            connectedGraph.setAdj(parent, i, 0);
-        }
-
-        int remainingEdges = numEdges - (numVertices - 1);
-
-        if (remainingEdges > 0) {
-            generateUnweightedEdges(connectedGraph,numVertices,remainingEdges);
+            generateEdges(connectedGraph,numVertices,remainingEdges,listWeight);
         }
 
         return connectedGraph;
